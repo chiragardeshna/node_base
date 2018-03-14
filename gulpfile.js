b587fb307c73462c118a7bdd353009236c97bc55
Dawie Harmse <dawie@mavenagency.co.za>
2018-03-14 15:29:42 +0530

    master: Automate gulp flow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,26 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var path = require("path");
+var nodemon = require('gulp-nodemon');
+var cache = require('gulp-cached');
 
 var tsProject = ts.createProject("./tsconfig.json");
 
-var srcFiles = [
-    // './src/*',
-    './src/**/*'
-    /*'./test/!*',
-    './test/!**!/!*',*/
-];
-
-gulp.task('scripts', function () {
-    return gulp.src(srcFiles)
-        .pipe(tsProject())
-        .pipe(gulp.dest('dist'));
+gulp.task('compile', function () {
+	return gulp.src('./src/**/*')
+	.pipe(cache('un_modified'))
+	.pipe(tsProject())
+	.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('watch', ['scripts'], function () {
-    gulp.watch(srcFiles)
-        .on('change', function (event) {
-            var file = event.path;
-            var destination = path.dirname(event.path.replace("/src/", "/dist/").replace(".ts", ".js"));
-            console.log('\x1b[42m', 'Changed --> ' + file);
-            return gulp.src(file).pipe(tsProject()).pipe(gulp.dest(destination));
-        });
+gulp.task('watch', ['compile'], function(){
+	return gulp.watch('./src/**/*', ['compile']);
+});
+
+gulp.task('dev', ['watch'], function(){
+	return nodemon({
+		script: 'dist/server.js',
+	}).on('restart', function(){
+		console.log('restarted');
+	});
 });
