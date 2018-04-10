ba2890eaaef89936fc0c1dc629bff8e600f71caf
Chirag Ardeshna <ardeshnachirag@gmail.com>
2018-04-10 17:51:34 +0530

    master: Add IOC Container

--- src/app/DI/DIBusinessLogic.ts
+++
@@ -1,9 +0,0 @@
-import User from "../Models/User";
-import Auth from "../BusinessLogic/Auth";
-
-export default (app) => {
-    app.container.register("Auth", () => {
-        return new Auth(User);
-    });
-    return app;
-}
